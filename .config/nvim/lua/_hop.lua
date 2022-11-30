e42fbbfc0a8f476a261b4db3f412e49e41acb2a5
Noobvelopper <38473290+Noobvelopper@users.noreply.github.com>
2022-11-30 21:46:43 +0900

    Change possible key for hop

--- .config/nvim/lua/_hop.lua
+++ .config/nvim/lua/_hop.lua
@@ -1,5 +1,5 @@
 -- place this in one of your configuration file(s)
-require('hop').setup{ keys = 'jfkdlséaurieowxc,mgh' }
+require('hop').setup{ keys = 'tnseriaoplfuwyx.c,vmgjdhbk' }
 local hop = require('hop')
 local directions = require('hop.hint').HintDirection
 -- vim.keymap.set('', 'f', function()
