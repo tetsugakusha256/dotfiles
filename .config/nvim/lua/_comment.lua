3af33bac7060b199d03884026ab5eeea898908e2
Noobvelopper <38473290+Noobvelopper@users.noreply.github.com>
2023-06-26 07:56:18 +0900

    changing block comment keymapping

--- .config/nvim/lua/_comment.lua
+++ .config/nvim/lua/_comment.lua
@@ -10,7 +10,7 @@ require('Comment').setup{
     ---Line-comment toggle keymap
     line = 'gcc',
     ---Block-comment toggle keymap
-    block = 'gbc',
+    block = 'gcb',
   },
   ---LHS of operator-pending mappings in NORMAL and VISUAL mode
   opleader = {
