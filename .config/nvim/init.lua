2c026d422ea691f2e8df7e107939c2a5638f0663
Noobvelopper <38473290+Noobvelopper@users.noreply.github.com>
2023-07-01 01:59:35 +0900

    moving auto format + adding diagnose function + only fomat js with prettier

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -143,8 +143,6 @@ vim.api.nvim_set_keymap("i", "<c-bs>", "<C-W>", { noremap = true })
 
 -- Save file
 vim.api.nvim_set_keymap("", "<c-s>", ":update<CR>", { noremap = true })
--- AutoFormat on save
-vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
 
 -- Maximize window
 vim.api.nvim_set_keymap("n", "<c-w>m", "<c-w>_ <c-w>|", { noremap = true })
--- .config/nvim/lua/_lspconfig.lua
+++ .config/nvim/lua/_lspconfig.lua
@@ -1,51 +1,77 @@
 require("mason").setup()
 require("mason-lspconfig").setup()
--- Setting up lsp server using lspconfig and mason-lspconfig
--- Mappings.
--- See `:help vim.diagnostic.*` for documentation on any of the below functions
+
+-- Diagnostic
 local opts = { noremap = true, silent = true }
-vim.keymap.set('n', '<space>d', vim.diagnostic.open_float, opts)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+vim.keymap.set("n", "<space>dd", vim.diagnostic.open_float, opts)
+vim.keymap.set("n", "<space>df", vim.diagnostic.setloclist, opts)
+vim.keymap.set("n", "<space>dh", vim.diagnostic.hide, opts)
+vim.keymap.set("n", "<space>ds", vim.diagnostic.show, opts)
+vim.keymap.set("n", "<c-[>", vim.diagnostic.goto_prev, opts)
+vim.keymap.set("n", "<c-]>", vim.diagnostic.goto_next, opts)
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
+  -- Formatting
+  local lsp_formatting = function()
+    vim.lsp.buf.format({
+      filter = function(client)
+        -- Don't format with tsserver
+        if client.name == "tsserver" then
+          return false
+        end
+        return true
+      end,
+      bufnr = bufnr,
+      async = true
+    })
+  end
+  -- If client can format create autocmd to format on save
+  if client.supports_method("textDocument/formatting") then
+    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = vim.api.nvim_create_augroup("LspFormatting", {}),
+      buffer = bufnr,
+      callback = function()
+        lsp_formatting(bufnr)
+      end,
+    })
+  end
   -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+  vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
   -- Mappings.
   -- See `:help vim.lsp.*` for documentation on any of the below functions
   local bufopts = { noremap = true, silent = true, buffer = bufnr }
-  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-  vim.keymap.set('n', 'gh', vim.lsp.buf.hover, bufopts)
-  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-  vim.keymap.set('n', '<space>ws', vim.lsp.buf.signature_help, bufopts)
-  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-  vim.keymap.set('n', '<space>wl', function()
+  vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+  vim.keymap.set("n", "gh", vim.lsp.buf.hover, bufopts)
+  vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set("n", "<space>ws", vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
+  vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set("n", "<space>wl", function()
     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
   end, bufopts)
-  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-  vim.keymap.set('n', '<space>fr', function() vim.lsp.buf.format { async = true } end, bufopts)
+  vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
+  vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+  vim.keymap.set("n", "<space>fr", lsp_formatting, bufopts)
 end
 
--- WARN: IF THE SERVER SEEM TO BE INSTALLED BUT DOESNT START CHECK NODE VERSION
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
-require("mason-lspconfig").setup_handlers {
+-- WARN: IF THE SERVER SEEM TO BE INSTALLED BUT DOESNT START CHECK NODE VERSION or server platform version
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+require("mason-lspconfig").setup_handlers({
   -- The first entry (without a key) will be the default handler
   -- and will be called for each installed server that doesn't have
   -- a dedicated handler.
   function(server_name) -- default handler (optional)
-    require("lspconfig")[server_name].setup {
+    require("lspconfig")[server_name].setup({
       -- Add lsp based completion to cmp_nvim for that server
       capabilities = capabilities,
       on_attach = on_attach,
-    }
+    })
   end,
   -- Next, you can provide a dedicated handler for specific servers.
   -- For example, a handler override for the `rust_analyzer`:
@@ -53,7 +79,7 @@ require("mason-lspconfig").setup_handlers {
   --     require("rust-tools").setup {}
   -- end
   ["lua_ls"] = function()
-    require('lspconfig')['lua_ls'].setup {
+    require("lspconfig")["lua_ls"].setup({
       capabilities = capabilities,
       on_attach = on_attach,
       -- ... other configs
@@ -61,10 +87,10 @@ require("mason-lspconfig").setup_handlers {
         Lua = {
           diagnostics = {
             -- Add vim to the global variables to avoid "vim not declared type of warning"
-            globals = { 'vim' }
-          }
-        }
-      }
-    }
-  end
-}
+            globals = { "vim" },
+          },
+        },
+      },
+    })
+  end,
+})
