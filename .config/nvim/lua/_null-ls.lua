6f1abf5e70852a82a1a1cf19ede70bb973876a52
Noobvelopper <38473290+Noobvelopper@users.noreply.github.com>
2023-07-01 01:57:53 +0900

    null-ls config

--- .config/nvim/lua/_null-ls.lua
+++ .config/nvim/lua/_null-ls.lua
@@ -1,12 +1,16 @@
+local null_ls = require("null-ls")
+-- mason-null-ls priority on null-ls
 require("mason-null-ls").setup({
   ensure_installed = {
     -- Opt to list sources here, when available in mason.
+    "prettierd",
   },
   automatic_installation = false,
   handlers = {},
 })
-require("null-ls").setup({
+null_ls.setup({
   sources = {
     -- Anything not supported by mason.
-  }
+    -- null_ls.builtins.formatting.prettierd
+  },
 })
