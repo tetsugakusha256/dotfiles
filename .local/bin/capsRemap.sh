3d2e845c1153e4510e73789395fd959d8fb83c9e
Noobvelopper <38473290+Noobvelopper@users.noreply.github.com>
2023-07-25 13:45:12 +0900

    Change to capslock instead of shiftlock option

--- .local/bin/capsRemap.sh
+++ .local/bin/capsRemap.sh
@@ -3,7 +3,7 @@
 # Define the program name
 program="xcape"
 
-/usr/bin/setxkbmap -option 'caps:ctrl_modifier shift:both_shiftlock'
+/usr/bin/setxkbmap -option 'caps:ctrl_modifier,shift:both_capslock_cancel'
 # Check if the program is already running
 if pgrep -x "$program" > /dev/null; then
   echo "$program is already running."
@@ -12,5 +12,5 @@ else
   echo "Launching $program..."
   # Replace the following line with the command you want to execute
 /usr/bin/xcape -e 'Caps_Lock=Escape' -t 200
-/usr/bin/xcape -e 'ISO_Level3_Shift=Control_L|BackSpace' -t 200
+# /usr/bin/xcape -e 'ISO_Level3_Shift=Control_L|BackSpace' -t 200
 fi
