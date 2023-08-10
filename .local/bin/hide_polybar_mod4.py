fef217b920ddd1c1419816b4571ad37d512d6138
Noobvelopper <38473290+Noobvelopper@users.noreply.github.com>
2023-08-10 17:58:36 +0900

    format

--- .local/bin/hide_polybar_mod4.py
+++ .local/bin/hide_polybar_mod4.py
@@ -8,14 +8,17 @@ last_pressed_time = time.time()
 time_threshold = 0.3
 # hide when login in
 os.system('xdotool search --class Polybar windowunmap %@')
+
+
 def on_press(key):
     global locked, time_pressed, last_pressed_time
     if key == Key.cmd:
         if not locked:
-            os.system('xdotool search --class Polybar windowmap %@ windowraise %@')
+            os.system(
+                'xdotool search --class Polybar windowmap %@ windowraise %@')
         if last_pressed_time + time_threshold > time.time():
-           time_pressed += 1
-           if time_pressed >= 3:
+            time_pressed += 1
+            if time_pressed >= 3:
                 locked = not locked
         else:
             time_pressed = 1
@@ -30,7 +33,7 @@ def on_release(key):
         else:
             if last_pressed_time + time_threshold < time.time():
                 time_pressed = 0
-            
+
 
 with Listener(
         on_press=on_press,
