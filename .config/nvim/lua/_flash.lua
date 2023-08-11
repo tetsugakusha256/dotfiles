9816fcc1343d4c03c8f0bb10e18fb425338e6f42
Noobvelopper <38473290+Noobvelopper@users.noreply.github.com>
2023-08-11 14:14:47 +0900

    disable flash search using / by default

--- .config/nvim/lua/_flash.lua
+++ .config/nvim/lua/_flash.lua
@@ -138,7 +138,7 @@ require('flash').setup({
     search = {
       -- when `true`, flash will be activated during regular search by default.
       -- You can always toggle when searching with `require("flash").toggle()`
-      enabled = true,
+      enabled = false,
       highlight = { backdrop = false },
       jump = { history = true, register = true, nohlsearch = true },
       search = {
