657225a97eef967d96abd07dfc25765b6908175c
Noobvelopper <38473290+Noobvelopper@users.noreply.github.com>
2023-10-10 20:31:45 +0900

    Remove battery full notification

--- .local/bin/battery_notification.sh
+++ .local/bin/battery_notification.sh
@@ -1,11 +1,12 @@
+# TODO: add memory, one notif only
 #!/bin/bash
 while true
 do
   battery_level=`acpi -b | grep -P -o '[0-9]+(?=%)'`
-   if [ $battery_level -ge 95 ]; then
-      notify-send "Battery Full" "Level: ${battery_level}%"
-      # paplay /usr/share/sounds/freedesktop/stereo/suspend-error.oga
-    elif [ $battery_level -le 20 ]; then
+   # if [ $battery_level -ge 95 ]; then
+   #    notify-send "Battery Full" "Level: ${battery_level}%"
+   #    # paplay /usr/share/sounds/freedesktop/stereo/suspend-error.oga
+    if [ $battery_level -le 15 ]; then
       notify-send --urgency=CRITICAL "Battery Low" "Level: ${battery_level}%"
       # paplay /usr/share/sounds/freedesktop/stereo/suspend-error.oga
   fi
