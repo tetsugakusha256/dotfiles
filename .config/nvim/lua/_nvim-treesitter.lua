44d4522c46ea32bb47512d0e28f6aaebf416dbb3
Noobvelopper <38473290+Noobvelopper@users.noreply.github.com>
2023-11-06 00:25:30 +0100

    markdown_inline added (not sure what it does...)

--- .config/nvim/lua/_nvim-treesitter.lua
+++ .config/nvim/lua/_nvim-treesitter.lua
@@ -1,6 +1,6 @@
 require 'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "lua", "rust", "javascript", "css", "html", "comment", "markdown" },
+  ensure_installed = { "lua", "rust", "javascript", "css", "html", "comment", "markdown", "markdown_inline" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
