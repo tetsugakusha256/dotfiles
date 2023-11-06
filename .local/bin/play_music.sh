2f33fc4ed4e43eceb78df1fc4530ca52a99ee1ff
Noobvelopper <38473290+Noobvelopper@users.noreply.github.com>
2023-11-06 02:27:27 +0100

    rename script

--- .local/bin/play_music.sh
+++ .local/bin/play_music.sh
@@ -4,8 +4,9 @@
 option1=append
 # play
 option2=play
-option3=append_mult
+option3=playlist
 music_dir="/home/anon/Documents/Music"
+playlist_dir="/home/anon/Documents/Music/Playlists"
 
 arg1=${1:-nothing}
 
@@ -42,17 +43,24 @@ elif [ $arg1 == $option2 ]; then
         echo "Playing : ${output}"
         selected_music_path="$music_dir$output"
         mocp -a "${selected_music_path}"
-        sleep 1 
         mocp -l "${selected_music_path}"
         echo "Added to playlist"
     fi
 elif [ $arg1 == $option3 ]; then
-    output=$(find /home/anon/Documents/Music -type f -iname "*.mp3" | fzf -m --bind=ctrl-e:up)
-    # foroutputmatted_selection=$(echo "$output" | awk -v ORS=' ' '{$1=$1}1' | sed 's/ $//' | sed -e 's/^/"/' -e 's/ /" "/g' -e 's/$/"/')
-    formatted_selection=$(echo "$output" | sed 's/ /\\ /g' | tr '\n' ' ')
-    echo "Adding : ${formatted_selection} to playlist"
-    mocp -a ${formatted_selection}
-    echo "Added"
+  #select playlist
+    playlist=$(find "$playlist_dir" -type f -iname "*.m3u" | sed "s#^$music_dir##" | rofi -dmenu -i -p "Choose playlist" \
+            -theme "$HOME"/.config/rofi/music_chooser.rasi\
+        )
+    if [ -z "$playlist" ]; then
+        echo "Variable is empty."
+    else
+        echo "Playlist : ${playlist}"
+        selected_music_path="$music_dir$playlist"
+        echo "Playlist : ${selected_music_path}"
+        mocp -c
+        mocp -a "${selected_music_path}"
+        echo "Added to playlist"
+    fi
 else
     echo "Invalide arguments"
 fi
