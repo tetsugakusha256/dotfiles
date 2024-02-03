c1aafa80d72c82d4fe143bfc047cc4a14adca0f9
Noobvelopper <38473290+Noobvelopper@users.noreply.github.com>
2024-02-03 05:04:37 +0100

    Added more color

--- .local/bin/i3lock-custom.sh
+++ .local/bin/i3lock-custom.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-i3lock --color=1e1e2edd --force-clock --radius=120 --ring-width=12 i3lock  -k --radius=120 --indicator --ring-width=15 --ringver-color=fab387 --ring-color=cba6f7 --ringwrong-color=f38ba8 --inside-color=181825 --insidever-color=fab387 --insidewrong-color=f38ba8 --keyhl-color=fab387 --bshl-color=cba6f7 --line-uses-ring --time-size=100 --time-color=fab387 --time-pos="850:200" --date-pos="850:280"  --date-color=fab387 --date-size=40 --verif-text="verifying 󰑓" --greeter-size=80 --greeter-font=HackNerdFontMono --time-font=HackNerdFontMono --date-font=HackNerdFontMono --wrong-font=HackNerdFontMono --verif-font=HackNerdFontMono --layout-font=HackNerdFontMono --verif-color=11111b --wrong-color=11111b --modif-color=11111b 
+i3lock --color=1e1e2edd --force-clock \
+    --radius=120 --ring-width=12 i3lock  -k --radius=120 --indicator \
+    --ring-width=15 --ringver-color=89dceb --ring-color=cba6f7 \
+    --ringwrong-color=f38ba8 --inside-color=181825 --insidever-color=89dceb \
+    --insidewrong-color=f38ba8 --keyhl-color=a6e3a1 --bshl-color=cba6f7 \
+    --line-uses-ring --time-size=100 --time-color=fab387 --time-pos="850:200" \
+    --date-pos="850:280"  --date-color=f38ba8 --date-size=40 --verif-text="verifying 󰑓" \
+    --greeter-size=80 --greeter-font=HackNerdFontMono --time-font=HackNerdFontMono \
+    --date-font=HackNerdFontMono --wrong-font=HackNerdFontMono \
+    --verif-font=HackNerdFontMono --layout-font=HackNerdFontMono \
+    --verif-color=11111b --wrong-color=11111b --modif-color=11111b
