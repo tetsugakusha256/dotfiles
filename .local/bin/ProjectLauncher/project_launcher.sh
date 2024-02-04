e073815af042a254aaf732922dd30a51f183936a
Noobvelopper <38473290+Noobvelopper@users.noreply.github.com>
2024-02-04 22:12:54 +0100

    Added project launcher script to setup an easy way to prepare workspace when working on specifc project

---
+++ .local/bin/ProjectLauncher/project_launcher.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Project to launch certain command into certain workspace on i3
+
+# if [ $# -lt 1 ]; then
+#     echo "Usage: $0 <Projects>"
+#     exit 1
+# fi
+
+PROJECTS_PATH="$HOME/.local/bin/ProjectLauncher/projects"
+
+choice=$(ls ${PROJECTS_PATH} | rofi -dmenu -i -p "Choose project" \
+        -theme "$HOME"/.config/rofi/music_chooser.rasi\
+    )
+if [ -z "$choice" ]; then
+    echo "Variable is empty."
+else
+    echo "Lauching: "$PROJECTS_PATH$choice
+    zsh $PROJECTS_PATH/$choice
+fi
