ae4d42852838c68f3a9767b07a5b295d5cebac2b
Noobvelopper <38473290+Noobvelopper@users.noreply.github.com>
2024-02-04 16:05:27 +0100

    Adding quick anki script to create anki card with mod+c

---
+++ .local/bin/QuickAnki/quick_anki.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+QUICK_CARD_PATH="$HOME/.local/bin/QuickAnki/quick_card.anki"
+
+
+if [ $# -gt 1 ]; then
+    echo "Usage: $0 <AnkiDeckName>"
+    exit 1
+fi
+
+if pgrep -x "anki" > /dev/null; then
+    echo "Anki already running."
+else
+    echo "Launching Anki"
+    anki&
+    disown
+    # Waiting for it to launch, maybe should find a way to ping anki-connect 
+    # in a loop or something, to have something more robust
+    sleep 2
+fi
+
+# Getting args
+anki_deck="$1"
+# If no arg, just open the quick card file on whatever it was last closed on
+if [ $# -eq 0 ]; then
+    alacritty -e nvim "$QUICK_CARD_PATH"
+# If arg given, load the template of the given deck
+else
+    alacritty -e nvim -c ":Anki $anki_deck" ./quick_card.anki
+fi
