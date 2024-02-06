80da9a1b380728b7d9482b1e8961eca54f2a4287
Noobvelopper <38473290+Noobvelopper@users.noreply.github.com>
2024-02-06 21:15:26 +0100

    Move to workspace 1

--- .local/bin/ProjectLauncher/projects/gameboyish.sh
+++ .local/bin/ProjectLauncher/projects/gameboyish.sh
@@ -3,3 +3,5 @@
 i3-msg 'workspace 1; exec /home/anon/.local/bin/ProjectLauncher/helper/gameboyish_ws1.sh'
 sleep 1
 i3-msg 'workspace 3; exec /home/anon/.local/bin/ProjectLauncher/helper/gameboyish_ws3.sh'
+sleep 1
+i3-msg workspace 1
