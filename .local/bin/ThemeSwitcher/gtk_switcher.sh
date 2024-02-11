a4306355ed1baa1fb07157f8a5a427d726449f72
Noobvelopper <38473290+Noobvelopper@users.noreply.github.com>
2024-02-11 06:16:52 +0100

    fix for anki and brave

--- .local/bin/ThemeSwitcher/gtk_switcher.sh
+++ .local/bin/ThemeSwitcher/gtk_switcher.sh
@@ -8,10 +8,13 @@ gtk_set_mocha () {
     sed -i 's/^gtk-application-prefer-dark-theme = .*/gtk-application-prefer-dark-theme = true/' $GTK_CONFIG_PATH
     sed -i 's/^gtk-theme-name = .*/gtk-theme-name = "Catppuccin-Mocha-Standard-Peach-Dark"/' $GTK_DOT_PATH
     sed -i 's/^gtk-application-prefer-dark-theme = .*/gtk-application-prefer-dark-theme = true/' $GTK_DOT_PATH
+    // Makes anki and brave work but need a restart still calibre not working
+    gsettings set org.gnome.desktop.interface gtk-theme Catppuccin-Latte-Standard-Peach-Dark:dark
 }
 gtk_set_latte () {
     sed -i 's/^gtk-theme-name = .*/gtk-theme-name = Catppuccin-Latte-Standard-Peach-Light/' $GTK_CONFIG_PATH
     sed -i 's/^gtk-application-prefer-dark-theme = .*/gtk-application-prefer-dark-theme = false/' $GTK_CONFIG_PATH
     sed -i 's/^gtk-theme-name = .*/gtk-theme-name = "Catppuccin-Latte-Standard-Peach-Light"/' $GTK_DOT_PATH
     sed -i 's/^gtk-application-prefer-dark-theme = .*/gtk-application-prefer-dark-theme = false/' $GTK_DOT_PATH
+    gsettings set org.gnome.desktop.interface gtk-theme Catppuccin-Latte-Standard-Peach-Light:light
 }
