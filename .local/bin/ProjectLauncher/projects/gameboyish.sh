e574bf361324d4b7367717a1aae8c76a0dc17e15
Noobvelopper <38473290+Noobvelopper@users.noreply.github.com>
2024-02-11 06:17:13 +0100

    tweak

--- .local/bin/ProjectLauncher/projects/gameboyish.sh
+++ .local/bin/ProjectLauncher/projects/gameboyish.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-i3-msg 'workspace 1; exec /home/anon/.local/bin/ProjectLauncher/helper/gameboyish_ws1.sh'
-sleep 1
 i3-msg 'workspace 3; exec /home/anon/.local/bin/ProjectLauncher/helper/gameboyish_ws3.sh'
 sleep 1
-i3-msg workspace 1
+i3-msg 'workspace 1; exec /home/anon/.local/bin/ProjectLauncher/helper/gameboyish_ws1.sh'
