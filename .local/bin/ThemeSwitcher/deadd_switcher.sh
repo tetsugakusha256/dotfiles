ca82fabf1209d51514f538bb4fbce03e7a7275e8
Noobvelopper <38473290+Noobvelopper@users.noreply.github.com>
2024-03-24 06:20:56 +0100

    changing paths

--- .local/bin/ThemeSwitcher/deadd_switcher.sh
+++ .local/bin/ThemeSwitcher/deadd_switcher.sh
@@ -2,12 +2,13 @@
 # Changes theme for my deadd-notification
 
 DEAD_CONFIG_PATH="$HOME/.config/deadd/deadd.css"
-DEAD_MOCHA_CONFIG_PATH="$HOME/.config/deadd/mocha-deadd.css"
-DEAD_LATTE_CONFIG_PATH="$HOME/.config/deadd/latte-deadd.css"
+DEAD_MOCHA_CONFIG_PATH=".config/catppuccin/css/mocha.css"
+DEAD_LATTE_CONFIG_PATH=".config/catppuccin/latte.css"
 
 deadd_set_mocha() {
     # Copy mocha file and rename it dead.css to make the change effective
-    cp $DEAD_MOCHA_CONFIG_PATH $DEAD_CONFIG_PATH
+    # cp $DEAD_MOCHA_CONFIG_PATH $DEAD_CONFIG_PATH
+    sed -i 's|^@import ".config/catppuccin/css/.*|@import ".config/catppuccin/css/mocha.css";|' $DEAD_CONFIG_PATH
     # turn highlighting off
     notify-send.py a --hint boolean:deadd-notification-center:true \
         int:id:1 boolean:state:false type:string:buttons
@@ -15,8 +16,9 @@ deadd_set_mocha() {
 }
 deadd_set_latte() {
     # Copy mocha file and rename it dead.css to make the change effective
-    cp $DEAD_LATTE_CONFIG_PATH $DEAD_CONFIG_PATH
+    # cp $DEAD_LATTE_CONFIG_PATH $DEAD_CONFIG_PATH
     # turn highlighting off
+    sed -i 's|^@import ".config/catppuccin/css/.*|@import ".config/catppuccin/css/latte.css";|' $DEAD_CONFIG_PATH
     notify-send.py a --hint boolean:deadd-notification-center:true \
         int:id:0 boolean:state:false type:string:buttons
     reload_deadd
