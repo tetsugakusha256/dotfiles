ee813521b39b34759ea8bf228de515d4ed62e4e1
Noobvelopper <38473290+Noobvelopper@users.noreply.github.com>
2024-05-14 22:30:03 +0200

    Theme switcher emacs support

--- .local/bin/ThemeSwitcher/emacs_switcher.sh
+++ .local/bin/ThemeSwitcher/emacs_switcher.sh
@@ -2,10 +2,13 @@
 # Changes theme for starship prompt
 
 DOOM_CONFIG_PATH="$HOME/.doom.d/config.el"
+EARLY_DOOM_CONFIG_PATH="$HOME/.emacs.d/early-init.el"
 
 emacs_set_mocha () {
   sed -i "s|^(setq catppuccin-flavor '.*)|(setq catppuccin-flavor 'mocha)|" $DOOM_CONFIG_PATH
+  sed -i 's|\(.*\)background-color \. ".*|\1background-color \. "#1E1E2E")|' $EARLY_DOOM_CONFIG_PATH
 }
 emacs_set_latte () {
   sed -i "s|^(setq catppuccin-flavor '.*)|(setq catppuccin-flavor 'latte)|" $DOOM_CONFIG_PATH
+  sed -i 's|\(.*\)background-color \. ".*|\1background-color \. "#EFF1F5")|' $EARLY_DOOM_CONFIG_PATH
 }
