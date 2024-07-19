485f228dfd128d1f1ed09238757ef64bbdeabc10
Noobvelopper <38473290+Noobvelopper@users.noreply.github.com>
2024-07-19 19:38:52 +0200

    rename project kernel in macro

--- .local/bin/ProjectLauncher/projects/kernel.sh
+++ .local/bin/ProjectLauncher/projects/kernel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-i3-msg 'workspace 3; exec /home/anon/.local/bin/ProjectLauncher/helper/launch_ws3_kl.sh'
+i3-msg 'workspace 3; exec /home/anon/.local/bin/ProjectLauncher/helper/launch_ws3_kernel.sh'
 sleep 1
-i3-msg 'workspace 1; exec /home/anon/.local/bin/ProjectLauncher/helper/launch_ws1_kl.sh'
+i3-msg 'workspace 1; exec /home/anon/.local/bin/ProjectLauncher/helper/launch_ws1_kernel.sh'
