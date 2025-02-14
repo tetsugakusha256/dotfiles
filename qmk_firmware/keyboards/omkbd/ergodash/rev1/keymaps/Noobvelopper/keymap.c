84d8fe3d94e459866719b58ee25e132e4c374b29
Noobvelopper <38473290+Noobvelopper@users.noreply.github.com>
2025-02-14 17:41:42 +0100

    keymap qmk

--- qmk_firmware/keyboards/omkbd/ergodash/rev1/keymaps/Noobvelopper/keymap.c
+++ qmk_firmware/keyboards/omkbd/ergodash/rev1/keymaps/Noobvelopper/keymap.c
@@ -33,11 +33,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_QWERTY] = LAYOUT(
-    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LBRC,                           KC_RBRC,     KC_6,     KC_7,   KC_8,    KC_9,    KC_0,    KC_BSPC,
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_MINS,                           KC_EQL ,     KC_Y,     KC_U,   KC_I,    KC_O,    KC_P,    KC_BSPC,
-    CTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_DEL ,                           KC_QUOT,     KC_H,     KC_J,   KC_K,    KC_L,    KC_SCLN,    KC_BSPC,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    LGUI(KC_5) ,                       LGUI(KC_6) , KC_N,     KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-    KC_LCTL, KC_MEH, KC_LGUI,   KC_LALT,              NUMBER, RAISE ,KC_ENT,                 KC_QUOT,     KC_ENT,   KC_SPC, LT(1,KC_SPC), RAISE, KC_RALT,   KC_ENT
+    KC_ESC,        KC_1,   KC_2,    KC_3,    KC_4,   KC_5,  KC_LBRC,    KC_RBRC,    KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_BSPC,
+    KC_TAB,        KC_Q,   KC_W,    KC_E,    KC_R,   KC_T,  KC_MINS,    KC_EQL,     KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_BSPC,
+    CTL_T(KC_ESC), KC_A,   KC_S,    KC_D,    KC_F,   KC_G,  KC_DEL,     KC_QUOT,    KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_BSPC,
+    KC_LSFT,       KC_Z,   KC_X,    KC_C,    KC_V,   KC_B,  LGUI(KC_5), LGUI(KC_6), KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+    KC_LCTL,       KC_MEH, KC_LGUI, KC_LALT, NUMBER, RAISE, KC_ENT,     KC_QUOT,    KC_ENT, KC_SPC, LT(1,    KC_SPC), RAISE,   KC_RALT, KC_ENT
   ),
 
   /* Lower
@@ -54,11 +54,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_NUMBER] = LAYOUT(
-    KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,   KC_BSLS,                        KC_PIPE, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10, KC_F12,
-    _______, KC_PPLS, KC_PSLS, KC_PAST, KC_PMNS,  KC_PPLS, KC_PEQL,                        KC_AMPR, KC_PERC, KC_COLN, KC_LCBR, KC_RCBR, KC_GT, KC_HASH,
-    _______, KC_1,    KC_2,    KC_3,    KC_4,     KC_5,    KC_PDOT,                        KC_CIRC, KC_EXLM, KC_QUES, KC_LPRN, KC_RPRN, KC_DLR,_______,
-    _______, KC_6,    KC_7,    KC_8,    KC_9,     KC_0,    _______ ,                       _______, KC_AT,   KC_TILD, KC_LBRC, KC_RBRC, KC_UNDS,_______,
-    _______, _______, _______,    _______,      EISU,   _______ ,_______,          _______,_______, _______,           EISU, _______, _______, _______
+    KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_BSLS, KC_PIPE, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,
+    _______, KC_PPLS, KC_PSLS, KC_PAST, KC_PMNS, KC_PPLS, KC_PEQL, KC_AMPR, KC_PERC, KC_COLN, KC_LCBR, KC_RCBR, KC_GT,   KC_HASH,
+    _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_PDOT, KC_CIRC, KC_EXLM, KC_QUES, KC_LPRN, KC_RPRN, KC_DLR,  _______,
+    _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, _______, KC_AT,   KC_TILD, KC_LBRC, KC_RBRC, KC_UNDS, _______,
+    _______, _______, _______, _______, EISU,    _______, _______, _______, _______, _______, EISU,    _______, _______, _______
   ),
 
   /* Raise
@@ -75,11 +75,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_RAISE] = LAYOUT(
-    _______, _______, _______,  _______, _______,  _______, _______,                        _______, _______,   KC_KB_VOLUME_DOWN,   KC_KB_VOLUME_UP,   KC_KB_MUTE,   _______,  _______,
-    _______, KC_BTN1, _______,  _______, KC_PASTE,  _______, _______,                        _______, _______, KC_PGDN, KC_PGUP, _______, _______, _______,
-    _______, _______, _______,  _______, _______,  _______, _______,                       _______, KC_LEFT,    KC_DOWN,    KC_UP,    KC_RIGHT,    _______, _______ ,
-    _______, _______, KC_CUT,  KC_COPY, _______,  _______, _______,                       _______, _______,    KC_WH_D,    KC_WH_U,    _______,   _______, _______,
-    _______, _______, _______,  _______,           _______, _______,_______,         _______,_______ , _______,            _______, _______, _______, _______
+    _______, _______, _______, _______, _______,  _______, _______, _______, _______, KC_KB_VOLUME_DOWN, KC_KB_VOLUME_UP, KC_KB_MUTE, _______, _______,
+    _______, KC_BTN1, _______, _______, KC_PASTE, _______, _______, _______, _______, _______,           KC_PGDN,         KC_PGDN,    _______, _______,
+    _______, _______, _______, _______, _______,  _______, _______, _______, KC_LEFT, KC_DOWN,           KC_UP,           KC_RIGHT,   _______, _______,
+    _______, _______, KC_CUT,  KC_COPY, _______,  _______, _______, _______, _______, KC_WH_D,           KC_WH_U,         _______,    _______, _______,
+    _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______,           _______,         _______,    _______, _______
   ),
 
   /* Adjust
@@ -96,11 +96,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_ADJUST] = LAYOUT(
-    _______, _______, _______, _______, _______, _______,_______,                       _______, _______, _______, _______, _______, _______, _______,
-    _______, QK_BOOT  , RGB_TOG, RGB_MOD, RGB_HUD, RGB_HUI,_______,                       _______, RGB_SAD, RGB_SAI, RGB_VAD, RGB_VAI, _______, _______,
-    _______, _______, BL_TOGG, BL_BRTG, BL_INC , BL_DEC ,_______,                       _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______,_______,                       _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______,          _______,_______,_______,       _______,_______, _______,          _______, _______, _______, _______
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, QK_BOOT, RGB_TOG, RGB_MOD, RGB_HUD, RGB_HUI, _______, _______, RGB_SAD, RGB_SAI, RGB_VAD, RGB_VAI, _______, _______,
+    _______, _______, BL_TOGG, BL_BRTG, BL_INC,  BL_DEC,  _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
   )
 };
 
